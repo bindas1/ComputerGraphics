2f84eef0cf91a54e3b65c0de9a5a42151b60d353
fan70m <fanychen@live.ca>
2020-03-03 18:35:40 +0100

    something

--- assignment_1/src/Cylinder.cpp
+++ assignment_1/src/Cylinder.cpp
@@ -33,28 +33,48 @@ intersect(const Ray&  _ray,
      * - store normal at _intersection_point in `_intersection_normal`.
      * - return whether there is an intersection with t > 0
     */
-  
-    //vec3 d = _ray.direction;
-    vec3 o = _ray.origin;
-    vec3 c = center;
-    vec3 a = axis; //todo make unitary 
-    double r = radius;
-    double h = height;
-
-
-    vec3 b = dot((o-c), a)*a ;//+ c;
     
-    const vec3 d = _ray.direction;
-    const vec3 ob = _ray.origin - b;
+
+    vec3 v = _ray.direction;
+    vec3 temp_v = (v - dot(v, axis) * axis); 	
+    int big_a = dot(temp_v, temp_v);
+    vec3 left = v-dot(v, axis)*axis;
+    vec3 delta_p = _ray.origin - center;
+    vec3 right = delta_p - dot(delta_p, axis) * axis;
+    int big_b = 2 *dot(left, right);
+    vec3 temp_v2 = delta_p - dot(delta_p, axis)*axis;
+    int big_c = dot(temp_v2, temp_v2) - radius*radius;
 
     std::array<double, 2> t;
     size_t number_of_solutions = solveQuadratic(
-        dot(d, d), // t^2 * ||d||^2
-        2 * dot(d, ob), // t * (2d dot (o - c))
-        dot(ob, ob) - radius * radius, // ||o-c||^2 - r^2
-        t // where to store solutions
+        big_a,
+	big_b,
+	big_c,
+	t // where to store solutions
     );
+
     
+    //    //vec3 d = _ray.direction;
+//    vec3 o = _ray.origin;
+//    vec3 c = center;
+//    vec3 a = axis; //todo make unitary 
+//    double r = radius;
+//    double h = height;
+//
+//
+//    vec3 b = dot((o-c), a)*a ;//+ c;
+//    
+//    const vec3 d = _ray.direction;
+//    const vec3 ob = _ray.origin - b;
+//
+//    std::array<double, 2> t;
+//    size_t number_of_solutions = solveQuadratic(
+//        dot(d, d), // t^2 * ||d||^2
+//        2 * dot(d, ob), // t * (2d dot (o - c))
+//        dot(ob, ob) - radius * radius, // ||o-c||^2 - r^2
+//        t // where to store solutions
+//    );
+//    
     // Initialize the intersection distance to "infinity" (indicating no intersection)
     _intersection_t = NO_INTERSECTION;
     
@@ -69,8 +89,8 @@ intersect(const Ray&  _ray,
     // Nothing to do if we don't have an intersection...
     if (_intersection_t == NO_INTERSECTION) return false;
     // Otherwise, calculate information about the intersection
-    _intersection_point = _ray(_intersection_t);
-    _intersection_normal = (_intersection_point - center) / radius;
+    _intersection_point = _ray.origin + _intersection_t*_ray.direction; 
+    _intersection_normal = _intersection_point - center;
 
     return true;
 }
