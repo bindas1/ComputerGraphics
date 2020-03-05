85cc840d459cf4df3203b2f4a01a21ef7c11d5b0
Gianni <gianni.lodetti@hotmail.com>
2020-03-05 13:50:55 +0100

    added plane.cpp and cylinder.cpp of old assignment

---
+++ assignment_2/build/src/CMakeFiles/debug_aabb.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Cylinder.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/Cylinder.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Image.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/Image.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Mesh.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/Mesh.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Plane.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/Plane.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Scene.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/Scene.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Sphere.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/Sphere.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/debug_aabb.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/debug_aabb.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/vec3.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/debug_aabb.dir/vec3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/lodePNG/SYSTEM"
+  "../lib/lodePNG/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/lib/lodePNG/CMakeFiles/lodePNG.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ assignment_2/build/src/CMakeFiles/raytrace.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Cylinder.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/Cylinder.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Image.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/Image.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Mesh.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/Mesh.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Plane.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/Plane.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Scene.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/Scene.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/Sphere.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/Sphere.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/raytrace.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/raytrace.cpp.o"
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/src/vec3.cpp" "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/src/CMakeFiles/raytrace.dir/vec3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/lodePNG/SYSTEM"
+  "../lib/lodePNG/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gianni/Desktop/GraphicsRepo/ComputerGraphics/assignment_2/build/lib/lodePNG/CMakeFiles/lodePNG.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
